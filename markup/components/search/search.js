d269a203b039565db90ad1c645716cbe3441681d
Dmytro <oberdev@gmail.com>
2017-04-02 23:18:47 +0300

    perfect scrollbar

--- markup/components/search/search.js
+++ markup/components/search/search.js
@@ -1,12 +1,18 @@
-import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar';
+// import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar';
+// import 'static/js/plugins/_jquery.scrollbar'
+import 'perfect-scrollbar/js/perfect-scrollbar.jquery.js'
 import 'jquery-mousewheel';
 
 $(document).ready(() => {
-  $('.js-scroll').mCustomScrollbar({
-    mouseWheel: { enable: true },
-    documentTouchScroll: true,
-    scrollInertia: 200,
-    scrollbarPosition: "outside",
+  // $('.js-scroll').mCustomScrollbar({
+  //   mouseWheel: { enable: true },
+  //   documentTouchScroll: true,
+  //   scrollInertia: 200,
+  //   scrollbarPosition: "inside",
+  //   theme: 'custom'
+  // });
+
+  $('.js-scroll').perfectScrollbar({
     theme: 'custom'
   });
 });
