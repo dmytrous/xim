17fb12de455303359e9b3240977452864ce96fa3
Dmytro <oberdev@gmail.com>
2017-04-04 08:42:45 +0300

    search results => apartments preview

--- markup/components/search/data/data.js
+++ markup/components/search/data/data.js
@@ -34,5 +34,67 @@ const data = {
         'Freezer',
         'Fridge'
     ]
-  }
+  },
+  results:[
+    {
+      "id": "1",
+      "name": "Stylish apartment in el born",
+      "price": "85",
+      "currency": "‌€",
+      "latitude": "41.393592",
+      "longitude": "2.162570",
+      "amentities": ["Guarded parking", "Balcony", "Dishwasher", "Freezer"],
+      "image": "a1.jpg"
+    },
+    {
+      "id": 2,
+      "name": "A cozy flat near Las Ramblas",
+      "price": "70",
+      "currency": "‌€",
+      "latitude": "41.394195",
+      "longitude": "2.164844",
+      "amentities": ["Free wireless internet", "Fireplace"],
+      "image": "a2.jpg"
+    },
+    {
+      "id": 3,
+      "name": "Apartment in the classic Barcelona center",
+      "price": "60",
+      "currency": "‌€",
+      "latitude": "41.393732",
+      "longitude": "2.165551",
+      "amentities": ["Air conditioner", "Elevator", "Terrace", "Blender", "Fridge"],
+      "image": "a3.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Classic Eixample place for rent",
+      "price": "75",
+      "currency": "‌€",
+      "latitude": "41.392794",
+      "longitude": "2.164888",
+      "amentities": ["Free Transportation", "Guarded parking", "Game room"],
+      "image": "a4.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Atlantida Beach",
+      "price": "90",
+      "currency": "‌€",
+      "latitude": "41.392876",
+      "longitude": "2.163260",
+      "amentities": ["Cooking hob"],
+      "image": "a5.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Elegant place in Eixample",
+      "price": "150",
+      "currency": "‌€",
+      "latitude": "41.394479",
+      "longitude": "2.163797",
+      "amentities": ["Air conditioner", "Computerr", "Free wireless internet", "Game room", "Terrace", "Fridge"],
+      "image": "a6.jpg"
+    }
+  ]
 };
