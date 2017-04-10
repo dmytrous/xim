49b740fd6f8bf499c2c6122489d053beb7c6591a
Dmytro <oberdev@gmail.com>
2017-04-10 09:07:38 +0300

    build

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap-sass/assets/javascripts/bootstrap/transition';
 import 'bootstrap-sass/assets/javascripts/bootstrap/collapse';
 import 'bootstrap-sass/assets/javascripts/bootstrap/tab';
-// import './plugins/modernizr-custom';
 import 'components/search/search';
 import 'components/search/result/map/map';
+// import './plugins/modernizr-custom';
+
